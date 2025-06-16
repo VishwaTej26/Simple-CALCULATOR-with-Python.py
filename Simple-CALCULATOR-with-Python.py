9212eefa13ad5b28728065c28c221d20dc7b27bd
VishwaTej26 <codelearnearn15lpa@gmail.com>
2025-06-16 21:54:00 +0530

    Create Simple-CALCULATOR-with-Python.py

---
+++ Simple-CALCULATOR-with-Python.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your a :"))
+b = int(input("Enter your b :"))
+cond = str(input("Enter your condition here :"))
+if cond == "add" :
+    print(a+b)
+if cond == "subtract":
+    print(a-b)
+if cond == "Multiply dude":
+    print(a*b)
+if cond == "Divide it":
+    print(a/b)
